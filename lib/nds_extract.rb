3647df266041282fc201ae8eb5a6baba23527623
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-11 18:32:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,7 @@ def total_gross(source)
   directors_index = 0
   total = 0
   while directors_index < list_of_directors(source).count do
-    total += directors_totals(source)[list_of_directors(source)]
+    total += directors_totals(source)[(source)list_of_directors]
     directors_index += 1 
   end
   
